fc18cc54fc006c63144241bf60b38f3be4f8f106
dtbinh <98450194+dtbinh1511@users.noreply.github.com>
2022-07-14 17:57:04 +0700

    update đăng ký
    cập nhật đăng ký tài khoản

---
+++ MyDB/Models/UserInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDB.Models
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+        public string Fullname { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public bool? Gender { get; set; }
+
+        public bool? Role { get; set; }
+
+        public string Slug { get; set; }
+
+        public DateTime? CreateAt { get; set; }
+
+        public int? CreateBy { get; set; }
+
+        public DateTime? UpdateAt { get; set; }
+
+        public int? UpdateBy { get; set; }
+
+        public int? Status { get; set; }
+    }
+}
--- websitebansach/Controllers/KhachhangController.cs
+++ websitebansach/Controllers/KhachhangController.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using MyDB.DAO;
 using MyDB.Models;
+using websitebansach.Library;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace websitebansach.Controllers
 {
@@ -30,7 +33,7 @@ namespace websitebansach.Controllers
             }
             else
             {
-                if (password.Equals(user.Password))
+                if (GetMD5(password).Equals(user.Password))
                 {
                     Session["CustomerAccount"] = user;
                     Session["CustomerId"] = user.Id;
@@ -50,11 +53,71 @@ namespace websitebansach.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DangKy(UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = userDAO.GetRow(userInfo.Email);
+                if (user == null)
+                {
+                    String pass = userInfo.Password;
+                    String confPass = userInfo.ConfirmPassword;
+                    if (pass.Equals(confPass))
+                    {
+                        user = new User();
+                        user.Email = userInfo.Email;
+                        user.Address = userInfo.Address;
+                        user.CreateAt = DateTime.Now;
+                        user.CreateBy = 1;
+                        user.Fullname = userInfo.Fullname;
+                        user.Gender = userInfo.Gender;
+                        user.Phone = userInfo.Phone;
+                        user.Role = false;
+                        user.Status = 1;
+                        user.Password = GetMD5(userInfo.Password);
+                        userDAO.Add(user);
+                        return RedirectToAction("Dangnhap", "Khachhang");
+
+                    }
+                    else
+                    {
+                        XMessage message = new XMessage("danger", "Mật khẩu không trùng khớp");
+                        return View("Dangky", "Khachhang");
+                    }
+                }
+                else
+                {
+                    XMessage message = new XMessage("danger", "Email đã tồn tại");
+                    return View("Dangky", "Khachhang");
+                }
+            }
+            return View();
+        }
         public ActionResult DangXuat()
         {
             Session["CustomerAccount"] = "";
             Session["MyCart"] = "";
             return Redirect("~/dang-nhap");
+        }
+
+
+
+        //create a string MD5
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
         }
     }
 }
